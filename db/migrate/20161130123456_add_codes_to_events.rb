fb8fb90bfb7bb3f5b529bd8d14248395e100c7bd
Mark Vella <markvella@Marks-MacBook.local>
2016-11-30 12:14:33 -0500

    Fixed migration

--- db/migrate/20161130123456_add_codes_to_events.rb
+++ db/migrate/20161130123456_add_codes_to_events.rb
@@ -1,4 +1,4 @@
-class AddCodeToEvents < ActiveRecord::Migration
+class AddCodesToEvents < ActiveRecord::Migration
   def change
   	add_column :events, :admin_invite_code, :string
   	add_column :events, :team_invite_code, :string
