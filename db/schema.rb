0fa63b557754438d50ef89f3cbcb53eb937d7e30
Sebastian Guerrero <sguerrer@andrew.cmu.edu>
2016-12-07 18:54:04 -0500

    migration 3

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161201123456) do
+ActiveRecord::Schema.define(version: 20161207123456) do
 
   create_table "assignments", force: :cascade do |t|
     t.integer  "event_id"
@@ -109,6 +109,7 @@ ActiveRecord::Schema.define(version: 20161201123456) do
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "name"
   end
 
   create_table "questions", force: :cascade do |t|
