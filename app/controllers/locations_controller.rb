53239c4ea53098fb70bb584550a1b09f85d37713
Mark Vella <markvella@Marks-MacBook.local>
2016-12-07 20:48:43 -0500

    Added name to required params

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -64,6 +64,6 @@ class LocationsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def location_params
-      params.require(:location).permit(:event_id, :description)
+      params.require(:location).permit(:event_id, :description, :name)
     end
 end
